6286b97ec07952d7df5e888699a80916fc28ecb0
benalcazar77 <43661756+benalcazar77@users.noreply.github.com>
2018-10-11 22:44:39 -0500

    Add files via upload

---
+++ app/Http/Controllers/dadosDP.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class dadosDP extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\DiceRoll  $diceRoll
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DiceRoll $diceRoll)
+    {
+        //
+    }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\DiceRoll  $diceRoll
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DiceRoll $diceRoll)
+    {
+        //
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\DiceRoll  $diceRoll
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, DiceRoll $diceRoll)
+    {
+        //
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\DiceRoll  $diceRoll
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DiceRoll $diceRoll)
+    {
+        //
+    }
+}
